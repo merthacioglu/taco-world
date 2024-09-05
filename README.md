87313c4d13f9738786bab1dd99f885b10436bcbb
Mert Hacioglu <mhacioglu96@gmail.com>
2024-09-05 01:07:30 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # taco-world
+
 A simple web app to create and order tacos
---
+++ src/main/java/org/mhacioglu/tacoworld/TacoWorldApplication.java
@@ -0,0 +1,13 @@
+package org.mhacioglu.tacoworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoWorldApplication.class, args);
+    }
+
+}
